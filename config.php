f58f4302aa5d62b206939f2c77a56660d9fd0e55
Plamen Penchew <syncmaster.plamen.penchew@abv.bg>
2017-01-06 17:02:54 +0200

    Fix coding style in config.php
    All lines of config.php, except first one, is changed.

--- config.php
+++ config.php
@@ -1,7 +1,6 @@
 <?php
-$connect = mysqli_connect ("localhost","root","","signupform");
-	mysqli_set_charset($connect ,'UTF8');
-	if (!$connect)
-		{
-			echo ("Connection error" . $connect->connect_error);
-		}
+$connect = mysqli_connect("localhost", "root", "", "signupform");
+mysqli_set_charset($connect, 'UTF8');
+if (!$connect) {
+	echo ("Connection error" . $connect->connect_error);
+}
