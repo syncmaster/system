677f0ca0ff783cf77c749c7bd6b758907aaf1e54
syncmaster <plamen.penchew@abv.bg>
2017-01-13 14:02:46 +0200

    remove unused files

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$connect = mysqli_connect("localhost", "root", "", "signupform");
-mysqli_set_charset($connect, 'UTF8');
-if (!$connect) {
-	echo ("Connection error" . $connect->connect_error);
-}
