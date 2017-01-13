992877f17ac7437ce900cc31ff99b46dc8d755a0
syncmaster <plamen.penchew@abv.bg>
2017-01-13 16:13:00 +0200

    new export of database

--- signup/signupform.sql
+++ signup/signupform.sql
@@ -23,6 +23,50 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура на таблица `friends`
+--
+
+CREATE TABLE `friends` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf16_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+--
+-- Схема на данните от таблица `friends`
+--
+
+INSERT INTO `friends` (`id`, `name`) VALUES
+(1, 'Plamen'),
+(2, 'Bogomil'),
+(3, 'Valentin'),
+(4, 'Mariya'),
+(5, 'Cvetomir'),
+(6, 'Teodora');
+
+-- --------------------------------------------------------
+
+--
+-- Структура на таблица `logininfo`
+--
+
+CREATE TABLE `logininfo` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `date` datetime NOT NULL,
+  `user_id` varchar(10) COLLATE utf16_unicode_ci NOT NULL,
+  `browser` varchar(32) COLLATE utf16_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+--
+-- Схема на данните от таблица `logininfo`
+--
+
+INSERT INTO `logininfo` (`id`, `date`, `user_id`, `browser`) VALUES
+(17, '2017-01-13 15:15:15', '79', 'Google Chrome'),
+(18, '2017-01-13 15:40:45', '79', 'Google Chrome');
+
+-- --------------------------------------------------------
+
+--
 -- Структура на таблица `users`
 --
 
@@ -43,16 +87,26 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `age`, `country`, `city`, `address`, `email`, `password`) VALUES
-(69, 'MArtin', 'Goranov', 23, 'Bulgaria', 'Pleven', 'pokalitin27', 'plamen@topnet.bg', 'f5bb0c8de146c67b44babbf4e6584cc0'),
-(75, 'Plamen', 'Penchev', 26, 'Bulgaria', 'Montana', 'montana 12', 'plamen1@topnet.bg', 'f5bb0c8de146c67b44babbf4e6584cc0'),
-(76, 'Plamen', 'Penchev', 25, 'Bulgaria', 'Tolbuhin', 'masdasdasd', 'wqesdaxcz@abv.bg', 'f5bb0c8de146c67b44babbf4e6584cc0'),
 (77, 'Bogomil', 'Dimitrov', 24, 'Bulgaria', 'Varna', 'podpolkovnik kalitin 27', 'bogomil@abv.bg', 'c8cdddae3fd642dadc4786eaf01e8264'),
-(78, 'Plamen', 'Penchev', 26, 'Serbia', 'Pliska', 'Preslav 15', 'gosho0@abv.bg', 'f5bb0c8de146c67b44babbf4e6584cc0'),
-(79, 'Plamen', 'Penchev', 24, 'Bulgaria', 'Tervel', 'maritsa 8', 'plamen@abv.bg', 'c8cdddae3fd642dadc4786eaf01e8264');
+(79, 'Plamen', 'Penchev', 24, 'Bulgaria', 'Tervel', 'maritsa 8', 'plamen@abv.bg', 'c8cdddae3fd642dadc4786eaf01e8264'),
+(80, 'Valentina', 'Angelova', 24, 'Bulgaria', 'Varna', 'kalitin 27', 'valq@abv.bg', 'c8cdddae3fd642dadc4786eaf01e8264'),
+(82, 'adadadasd', 'asdasdsadsa', 21, 'England', 'asdasdasdad', 'asdasdasdasd', 'plamen232213231@abv.bg', '25f9e794323b453885f5181f1b624d0b');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `friends`
+--
+ALTER TABLE `friends`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `logininfo`
+--
+ALTER TABLE `logininfo`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -65,10 +119,15 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `logininfo`
+--
+ALTER TABLE `logininfo`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
