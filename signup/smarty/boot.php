50fba45666db35b2525807d51ae3cdf45369b45f
syncmaster <plamen.penchew@abv.bg>
2017-01-20 17:03:16 +0200

    remove info from boot and added to settings

--- signup/smarty/boot.php
+++ signup/smarty/boot.php
@@ -1,9 +1,17 @@
 <?php
-$connect = new mysqli("localhost", "root", "", "signupform");
+include("settings.php");
+require("libs/Smarty.class.php");
+@ini_set('session.hash_function', 1);
+@ini_set('session.hash_bits_per_character', 6);
+session_start();
+
+$smarty = new Smarty();
+
+$smarty->error_reporting = error_reporting() &~E_NOTICE;
+
+
+$connect = new mysqli($host_db, $user_db, $password_db, $database);
 mysqli_set_charset($connect, 'UTF8');
 if ($connect->connect_error){
 	echo ("Connection Error:" . $connect->connect_error);
 }
-@ini_set('session.hash_function', 1);
-@ini_set('session.hash_bits_per_character', 6);
-session_start();
---
+++ signup/smarty/settings.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'phpmail/PHPMailerAutoload.php';
+//database info
+$database = "signupform";
+$user_db = "root";
+$password_db = "";
+$host_db = "localhost";
+
+
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+$mail->Host = 'smtp.gmail.com';
+$mail->Username = 'plamenp6@gmail.com';
+$mail->Password = '0899068628paco';
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
+$mail->From = 'support@domain.com';
+$mail->FromName = 'Admin';
+$mail->addReplyTo('reply@domain.com', 'Admin');
+$mail->IsHTML(true);
+$mail->CharSet="utf-8";	
+
